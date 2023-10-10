5b14b2e910730f91fa4fe4ec3e28a8bf00c59370
Idergo <rianhougan@gmail.com>
2023-10-10 22:58:21 +0300

    aaaa

---
+++ script.js
@@ -0,0 +1,12 @@
+const age = prompt("You age are...");
+
+if (age >=18) {
+    console.log("You can watch this video!")
+} else {
+    const numberYersLeft = 18 - age;
+    let endFrase;
+
+
+    if (numberYersLeft > 1) {endFrase = "years"} else {endFrase = "year"}
+    console.log(`You can watch this X, after ${numberYersLeft} ${endFrase}!`)
+}
