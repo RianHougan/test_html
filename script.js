3f7a4e067cd4134be352b5bae4ac43c4ba6cfe1e
Idergo <rianhougan@gmail.com>
2023-10-17 12:01:17 +0300

    Codewars Строка в массив чисел наоборот

--- script.js
+++ script.js
@@ -1,25 +1,4 @@
-const bils = [11, 20, 47]
-// const tips = []
-const totalBils = []
-
-const calculateTips = (tips) => (tips >= 20) ? Number((tips/100*20).toFixed(2)) : Number((tips/100*15).toFixed(2))
-const tips = bils.map(calculateTips)
-
-
-for (let i = 0;i < bils.length; i++) {
-    totalBils.push(bils[i]+tips[i])
+function digitize(n) {
+    return String(n).split('').map(Number).reverse()
 }
-
-let newBody = `
-    <p>${bils.join(', ')}</p>
-<!--    <br>-->
-    <p>${tips.join(', ')}</p>
-<!--    <br>-->
-    <p>${totalBils.join(', ')}</p>
-`
-document.body.innerHTML = newBody
-console.log(bils)
-console.log(tips)
-console.log(totalBils)
-
-// console.log(calculateTips(chek[1]))
+console.log(digitize(35231))
